87e0f9dd668a79c14838494a586bcd550e24551a
Matthew Stankiewicz <matthew.stankiewicz@gmail.com>
2026-03-24 10:26:51 -0400

    Update api.js

--- api.js
+++ api.js
@@ -1,6 +1,19 @@
+function getUserKey() {
+  let key = localStorage.getItem("workout_key");
+
+  if (!key) {
+    key = prompt("Enter your key:");
+    if (key) {
+      localStorage.setItem("workout_key", key);
+    }
+  }
+
+  return key;
+}
+
 function getScriptURL() {
-  const userKey = localStorage.getItem("workout_key");
-  return `https://script.google.com/macros/s/AKfycbxAP0EAtNUCVj97wxyUZJqQf1XHlv9XFFbbit1HBLIWY-8t0c1DZWqS-PXBDuSGB5jX5Q/exec?key=${userKey}`;
+  const key = getUserKey();
+  return `https://script.google.com/macros/s/AKfycbxAP0EAtNUCVj97wxyUZJqQf1XHlv9XFFbbit1HBLIWY-8t0c1DZWqS-PXBDuSGB5jX5Q/exec?key=${key}`;
 }
 
 async function sendLog(data) {
