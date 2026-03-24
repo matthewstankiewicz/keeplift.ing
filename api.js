ab08bb428069be9fcbf94cbf77b751088ed93dc7
Matthew Stankiewicz <matthew.stankiewicz@gmail.com>
2026-03-24 09:39:55 -0400

    Create api.js

---
+++ api.js
@@ -0,0 +1,27 @@
+const scriptURL = `https://script.google.com/macros/s/AKfycbxAP0EAtNUCVj97wxyUZJqQf1XHlv9XFFbbit1HBLIWY-8t0c1DZWqS-PXBDuSGB5jX5Q/exec?key=${userKey}`;
+
+async function sendLog(data) {
+  return fetch(scriptURL, {
+    method: "POST",
+    body: JSON.stringify(data)
+  });
+}
+
+function queueLog(data) {
+  const q = JSON.parse(localStorage.getItem("queue") || "[]");
+  q.push(data);
+  localStorage.setItem("queue", JSON.stringify(q));
+}
+
+async function flushQueue() {
+  const q = JSON.parse(localStorage.getItem("queue") || "[]");
+  if (!q.length) return;
+
+  for (const item of q) {
+    await sendLog(item);
+  }
+
+  localStorage.removeItem("queue");
+}
+
+window.addEventListener("online", flushQueue);
